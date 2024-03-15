2e01c24c8e1093cd7c6e74d9f730bb6ee1e13c39
Pakpoom Teedindam <43717543+basehappy19@users.noreply.github.com>
2024-03-15 21:07:02 +0700

    Add data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,145 @@
+import { useState } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState("");
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const getEmployees = () => {
+    Axios.get('http://localhost:3001/employees').then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage
+        }
+      ])
+      })
+    }
+
   return (
     <>
       <div className="container">
         <h1>Employee Information</h1>
         <div className="information">
           <form action="">
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">
-                Name
-              </label>
-              <input type="text" name="name" className="form-control" placeholder="Enter Name" />
+            <div className="text-start">
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  placeholder="Enter Name"
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="age" className="form-label">
+                  Age
+                </label>
+                <input
+                  type="text"
+                  name="age"
+                  className="form-control"
+                  placeholder="Enter Age"
+                  onChange={(event) => {
+                    setAge(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="country" className="form-label">
+                  Country
+                </label>
+                <input
+                  type="text"
+                  name="country"
+                  className="form-control"
+                  placeholder="Enter country"
+                  onChange={(event) => {
+                    setCountry(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="position" className="form-label">
+                  Position
+                </label>
+                <input
+                  type="text"
+                  name="position"
+                  className="form-control"
+                  placeholder="Enter position"
+                  onChange={(event) => {
+                    setPosition(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="wage" className="form-label">
+                  Wage
+                </label>
+                <input
+                  type="number"
+                  name="wage"
+                  className="form-control"
+                  placeholder="Enter wage"
+                  onChange={(event) => {
+                    setWage(event.target.value);
+                  }}
+                />
+              </div>
             </div>
-            <div className="mb-3">
-              <label htmlFor="age" className="form-label">
-                Age
-              </label>
-              <input type="text" name="age" className="form-control" placeholder="Enter Age" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="country" className="form-label">
-                Country
-              </label>
-              <input type="text" name="country" className="form-control" placeholder="Enter country" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="position" className="form-label">
-                Position
-              </label>
-              <input type="text" name="position" className="form-control" placeholder="Enter position" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="wage" className="form-label">
-                Wage
-              </label>
-              <input type="number" name="wage" className="form-control" placeholder="Enter wage" />
-            </div>
-            <button className="btn btn-success">Add Employee</button>
+            <button className="btn btn-success" onClick={addEmployee}>Add Employee</button>
           </form>
         </div>
         <hr />
         <div className="employees">
-          <button className="btn btn-primary">Show employees</button>
+          <button className="btn btn-primary" onClick={getEmployees}>
+            Show employees
+          </button>
+          <br />
+          <br />
+          {employeeList.map((val, key) => {
+            return (
+              <div className="employee card text-start">
+                <div className="card-body">
+                  <p className="card-text">Name: {val.name}</p>
+                  <p className="card-text">Age: {val.age}</p>
+                  <p className="card-text">Country: {val.country}</p>
+                  <p className="card-text">Position: {val.position}</p>
+                  <p className="card-text">Wage: {val.wage}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- server/index.js
+++ server/index.js
@@ -23,6 +23,26 @@ app.get('/employees', (reg, res) => {
     });
 });
 
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query(
+        "INSERT INTO employees (name, age, country, position, wage) VALUES(?,?,?,?,?)",
+        [name, age, country, position, wage],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Values inserted");
+            }
+        }
+    );
+});
+
 app.listen('3001', () => {
     console.log('Server Is Runing On Port 3001')
 })
